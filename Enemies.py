982594d3536e531d64e3682040e00fa621199405
NineSided <Ninesideddevelopment@gmail.com>
2024-02-04 09:23:43 +0000

    Update--Enemies-&-Main

--- Enemies.py
+++ Enemies.py
@@ -1,1 +1,11 @@
 import pygame
+pygame.init()
+
+class SquareEnemy:
+    def __init__(self, position, rotation):
+    	self.position = position
+    	self.rotation = rotation
+
+    def pointToTarget(target):
+    	targetX, targetY = target.position[0], target.position[1]
+    	
